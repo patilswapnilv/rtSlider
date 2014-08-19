56123e8fe410f613b68525b7ca62714d8ffcae75
Swapnil V. Patil <patilswapnilv@gmail.com>
2014-08-19 13:08:20 +0530

    Update

--- single-rtslider.php
+++ single-rtslider.php
@@ -4,7 +4,6 @@
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<?php  the_content(); ?>
 		<?php echo do_shortcode('[rtSlider id="'.$post->ID.'"]'); ?>
-		<?php echo do_shortcode('[rtSlider id="4"]');?>
 		<!-- echo '[rtSlider id="'.$post->ID.'"]'; -->
 	<?php endwhile; ?>			
 			
