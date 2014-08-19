d881d70c41ab7c02b93de4bb0cb51a096bd78d45
Swapnil V. Patil <patilswapnilv@gmail.com>
2014-08-19 14:38:14 +0530

    Changed Single.php Experimenting

--- single.php
+++ single.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+/*<?php get_header(); ?>
 
 
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
@@ -8,4 +8,28 @@
 	<?php endwhile; ?>			
 			
 
+<?php get_footer(); ?>*/
+
+<?php
+/**
+ * The Template for displaying all Single Project posts.
+ *
+ * @package WordPress
+ * @subpackage Laurel Streng
+ * @since Laurel Streng 2.0
+ */
+ 
+get_header(); ?>
+ 
+<?php while ( have_posts() ) : the_post(); ?>
+ 
+        <?php if ( is_single() ) : ?>
+ 
+                <div class="row" role="main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  		echo'rtslider' .$post->ID;
+        <?php endif; // is_single() ?>
+ 
+<?php endwhile; ?>
+ 
 <?php get_footer(); ?>
